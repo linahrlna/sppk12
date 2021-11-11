73b501ec02c5ba3281e26ca453019849cb041a3b
linahrlna <linaherlinaa142@gmail.com>
2021-11-11 14:40:53 +0700

    Create arc.php

---
+++ application/config/arc.php
@@ -0,0 +1,41 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/*
+|--------------------------------------------------------------------------
+| Media
+|--------------------------------------------------------------------------
+| Konfigurasi untuk lokasi media
+| 
+|
+*/
+$config['media_url'] = '-';
+
+/*
+|--------------------------------------------------------------------------
+| Setting
+|--------------------------------------------------------------------------
+|
+| 
+|
+*/
+$config['app_name'] = 'SPP-PONPES';
+$config['version'] = 'V.3.0';
+$config['created'] = 'Copyright © 2017-'.date('Y').' <a target="_blank" href="#">Gold Lion Networks International</a>. All rights reserved.';
+
+/*
+|--------------------------------------------------------------------------
+| Email
+|--------------------------------------------------------------------------
+|
+| Configuration email to send email notification
+|
+*/
+$config['email'] = TRUE;
+
+/*
+|--------------------------------------------------------------------------
+| Email disclaimer
+|--------------------------------------------------------------------------
+|
+*/
+$config['email_disclaimer'] = "\n\n<em>Email ini dikirim secara otomatis, mohon untuk tidak membalas email ini. Jika mengalami kendala teknis maka dapat menghubungi <a href=\"mailto:\">email@gmail.com</a></em>";
