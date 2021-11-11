61cc225dc81c33e1646de7c5184053e2651bb074
linahrlna <linaherlinaa142@gmail.com>
2021-11-11 19:20:21 +0700

    Create portal.php

---
+++ application/views/portal.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<meta name="description" content="">
+	<meta name="author" content="">
+
+	<title>PEMBAYARAN SPP | UTAMA </title>
+	<link rel="icon" type="image/png" href="<?php echo media_url('img/logosmk12.png') ?>">
+
+	<!-- Bootstrap Core CSS -->
+	<link href="<?php echo media_url() ?>/css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="<?php echo media_url() ?>/css/load-font-googleapis.css">
+	<!-- Font Awesome -->
+	<link rel="stylesheet" href="<?php echo media_url() ?>/css/font-awesome.min.css">
+
+
+	<!-- Custom CSS -->
+	<link href="<?php echo media_url() ?>/css/frontend-style.css" rel="stylesheet">
+	<link href="<?php echo media_url() ?>/css/portal.css" rel="stylesheet">
+
+</head>
+
+<body>
+
+	<!-- Home -->
+<body style="background-image:#fffff;background-size:cover;">
+    <div class="page-wrapper">
+        <div class="page-content--bge5">
+            <div class="container">
+                <div class="login-wrap">
+                    <div class="login-content">
+	<section class="content-section">
+		<div class="container text-center">
+			
+			<div class="row">
+				<div class="col-md-12">
+					<h1><i class="fa fa-graduation-cap"></i> SELAMAT DATANG</h1>
+					<p class="lead mb-5 colr">SISTEM PEMBAYARAN SPP SMKN 12 MALANG</p>
+				</div>	
+				
+					
+				<div class="col-md-6 float-center" >
+					<a href="<?php echo site_url('manage') ?>">
+					<div class="box text-center">
+						<i class="fa fa-desktop icon-menu"></i>
+						<br>
+						Login Petugas
+					</div>
+				</a>
+				</div>
+				<div class="col-md-6 float-center">
+					<a href="<?php echo site_url('home') ?>">
+					<div class="box text-center">
+						<i class="fa fa-credit-card icon-menu"></i>
+						<br>
+						Cek Pembayaran Siswa
+					</div>
+				</a>
+				</div>
+
+                </div>
+			</div>
+		</div>
+
+	</section>
+
+
+</body>
+
+</html>
