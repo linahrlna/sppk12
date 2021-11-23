68e6f65cbcf081e9b1f71899e227245aee1194a2
keyshanabila12 <keyshakeysha813@gmail.com>
2021-11-22 23:08:39 -0800

    Update Maintenance_set_petugas.php

--- application/modules/maintenance/controllers/Maintenance_set_petugas.php
+++ application/modules/maintenance/controllers/Maintenance_set_petugas.php
@@ -6,20 +6,20 @@ class Maintenance_set_petugas extends CI_Controller {
 	public function __construct() {
 		parent::__construct(TRUE);
 		if ($this->session->userdata('logged') == NULL) {
-			header("Location:" . site_url('manage/auth/login') . "?location=" . urlencode($_SERVER['REQUEST_URI']));
+			header("Location:" . site_url('petugas/auth/login') . "?location=" . urlencode($_SERVER['REQUEST_URI']));
 		}
-		$this->load->model(array('setting/Setting_model'));
+		$this->load->model(array('setting/Setting_model_petugas'));
 	}
 
     // kredit view in list
 	public function index() {
 
-		$config['base_url'] = site_url('manage/maintenance/index');
+		$config['base_url'] = site_url('petugas/maintenance/index');
 		$config['suffix'] = '?' . http_build_query($_GET, '', "&");
 
 		$data['title'] = 'Maintenance';
 		$data['main'] = 'maintenance/maintenance_list_petugas';
-		$this->load->view('manage/layout', $data);
+		$this->load->view('petugas/layout', $data);
 	}
 
 	public function backup() {
