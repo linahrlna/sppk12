a411583cece83b2dd1e87fed676a467f19e7006d
keyshanabila12 <keyshakeysha813@gmail.com>
2021-11-24 23:32:29 -0800

    Update Student_petugas.php

--- application/modules/student/controllers/Student_petugas.php
+++ application/modules/student/controllers/Student_petugas.php
@@ -151,7 +151,7 @@ class Student_petugas extends CI_Controller {
 
     // Delete to database
   public function delete($id = NULL) {
-    if ($this->session->userdata('uroleid')!= SUPERUSER){
+    if ($this->session->userdata('uroleid')!= EXTRAUSER){
       redirect('petugas');
     }
     if ($_POST) {
