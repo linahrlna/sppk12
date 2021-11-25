bea7a19a07762915ce9da0524a643befd42fb68c
keyshanabila12 <keyshakeysha813@gmail.com>
2021-11-24 21:05:31 -0800

    Update Majors_set_petugas.php

--- application/modules/majors/controllers/Majors_set_petugas.php
+++ application/modules/majors/controllers/Majors_set_petugas.php
@@ -114,7 +114,7 @@ class Majors_set extends CI_Controller {
 
 // Delete to database
   public function delete($id = NULL) {
-    if ($this->session->userdata('uroleid')!= SUPERUSER){
+    if ($this->session->userdata('uroleid')!= EXTRAUSER){
       redirect('petugas');
     }
     if ($_POST) {
